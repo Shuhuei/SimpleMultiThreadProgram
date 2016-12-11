3d22eb1c850e5fc77df7c0160b0da72061dabb83
shuhuei <shumach5@gmail.com>
2016-12-11 22:18:56 +0900

    initial commit

--- SimpleMultiThreadProgram/Program.cs
+++ SimpleMultiThreadProgram/Program.cs
@@ -10,6 +10,21 @@ namespace SimpleMultiThreadProgram
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Main thread (thread id:{0}) starting", Thread.CurrentThread.ManagedThreadId);
+
+            Action ac = () => {
+                Console.WriteLine("Another thread (thread id: {0}) starting", Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(5000);
+                Console.WriteLine("Another thread (thread id: {0}) ended", Thread.CurrentThread.ManagedThreadId);
+            };
+
+            for (int i = 0; i < 15; i++)
+            {
+                Task.Run(ac);
+            }
+
+            Console.WriteLine("Main thread (thread id:{0}) ended", Thread.CurrentThread.ManagedThreadId);
+            Console.Read();
         }
     }
 }
